7224244d001271d6c21f52a3d86023a838dc37aa
Rahul-Sakthi <114357527+Rahul-Sakthi@users.noreply.github.com>
2022-09-28 11:36:53 +0530

    Add files via upload

---
+++ DemosocgenApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosocgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosocgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosocgenApplication.class, args);
+	}
+
+}
